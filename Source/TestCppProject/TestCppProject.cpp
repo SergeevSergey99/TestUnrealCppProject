5a46314d7c4f75c924c4926df377807d73cb9631
Sergeev Sergey <Sergeev_sergey99@mail.ru>
2026-07-02 15:02:01 +0300

    init

---
+++ Source/TestCppProject/TestCppProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestCppProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestCppProject, "TestCppProject" );
